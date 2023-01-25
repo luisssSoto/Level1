70a89130f719c1c666729cb8512d31a24adbf39f
luisSoto <alejandrosotoserafin@gmail.com>
2023-01-25 17:53:48 -0600

    is this the end?

--- InReverse.py
+++ InReverse.py
@@ -1,8 +1,11 @@
 #Build a pseudocode and flowchart which has 4 integers data A,B,C,D and then write these data in reverse
-
+#Comenario!!!
 a=input('Enter the value of A: ')
 b=input('Enter the value of B: ')
 c=input('Enter the value of C: ')
 d=input('Enter the value of D: ')
 
-print('Here is your values in reverse: ', d, ' ', c, ' ', b, ' ', a)
+right=[a,b,c,d]
+print(right)
+right.reverse()
+print('Here is your values in reverse: ', right)
